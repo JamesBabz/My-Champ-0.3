43010e428f666f727f368e81a348078f207b78b7
Enemark96 <je.enemark96@gmail.com>
2017-01-11 14:02:24 +0100

    Fix af controller til nyt view
    Fix af controller til nyt view

--- src/mychamp/gui/controller/FinalsViewController.java
+++ src/mychamp/gui/controller/FinalsViewController.java
@@ -7,7 +7,10 @@ package mychamp.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -15,6 +18,49 @@ import javafx.fxml.Initializable;
  * @author Jacob Enemark
  */
 public class FinalsViewController implements Initializable {
+
+    @FXML
+    private TableView<?> quarterFinalA;
+    @FXML
+    private TableColumn<?, ?> QuarterTeamA;
+    @FXML
+    private TableColumn<?, ?> QuarterScoreA;
+    @FXML
+    private TableView<?> quarterFinalB;
+    @FXML
+    private TableColumn<?, ?> QuarterTeamB;
+    @FXML
+    private TableColumn<?, ?> QuarterScoreB;
+    @FXML
+    private TableView<?> SemiFinalA;
+    @FXML
+    private TableColumn<?, ?> SemiTeamA;
+    @FXML
+    private TableColumn<?, ?> SemiScoreA;
+    @FXML
+    private TableView<?> FinalA;
+    @FXML
+    private TableColumn<?, ?> FinalTeamA;
+    @FXML
+    private TableColumn<?, ?> FinalScoreA;
+    @FXML
+    private TableView<?> quarterFinalC;
+    @FXML
+    private TableColumn<?, ?> QuarterTeamC;
+    @FXML
+    private TableColumn<?, ?> QuarterScoreC;
+    @FXML
+    private TableView<?> SemiFinalB;
+    @FXML
+    private TableColumn<?, ?> SemiTeamB;
+    @FXML
+    private TableColumn<?, ?> SemiScoreB;
+    @FXML
+    private TableView<?> quarterFinalD;
+    @FXML
+    private TableColumn<?, ?> QuarterTeamD;
+    @FXML
+    private TableColumn<?, ?> QuarterScoreD;
 
     /**
      * Initializes the controller class.
